b31b017271fc96455413271f1c0dbb182fa91f38
Bateristico <camilo.saa@gmail.com>
2022-02-24 13:36:38 +0100

    add ResultsDetail component

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View>
+      <Text>{Result.name}</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default ResultsDetail;
